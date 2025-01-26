58b8f716c1b183923b3fdc54029023bedd0f2dc7
pnhoanganh <pnhoanganhh@gmail.com>
2025-01-27 00:01:17 +0700

    fix UI

--- src/components/Note/NoteContainer.jsx
+++ src/components/Note/NoteContainer.jsx
@@ -4,11 +4,11 @@ import TakeNote from './TakeNote'
 
 function NoteContainer({ activeNote }) {
   return (
-    <div className="flex flex-col h-screen w-full p-4 pt-8 bg-white text-black">
+    <div className="flex flex-col h-screen w-full p-6 pt-9 bg-white text-black">
       <div className="mb-4">
         <NoteHeader />
       </div>
-      <div className=" flex-grow border rounded-lg p-4">
+      <div className=" pt-2">
         <TakeNote />
       </div>
     </div>
--- src/components/Note/NoteHeader.jsx
+++ src/components/Note/NoteHeader.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function NoteHeader() {
   return (
-    <header className="min-h-[2rem] items-center  w-full flex pb-3 border-b-[1px] border-light-secondary-40">
+    <header className="min-h-[2rem] items-center  w-full flex pb-4 border-b-[1px] border-light-secondary-40">
       <div className="relative w-full">
         <label
           htmlFor="name"
-          className="absolute -top-2 left-2 inline-block rounded-lg bg-white px-1 text-xs font-medium text-gray-900"
+          className="absolute -top-4 left-2 inline-block rounded-lg bg-white px-1 text-[16px] font-medium text-light-primary-50"
         >
           Note Title
         </label>
@@ -15,7 +15,7 @@ function NoteHeader() {
           name="name"
           type="text"
           placeholder="Enter title of your note..."
-          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-secondary sm:text-sm/6"
+          className="block w-full rounded-md bg-white px-3 py-2.5 text-lg text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 placeholder:text-[16px] focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-secondary "
         />
       </div>
     </header>
